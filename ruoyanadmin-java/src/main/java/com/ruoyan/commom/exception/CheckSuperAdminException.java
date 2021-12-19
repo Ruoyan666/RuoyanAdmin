bd15ee17e42cb95c6befb27cc0e89c1c8ee6e853
ruoyan <java@ruoyan.com>
2021-12-19 11:18:12 +0800

    新增了检查超级管理员身份异常类

---
+++ ruoyanadmin-java/src/main/java/com/ruoyan/commom/exception/CheckSuperAdminException.java
@@ -0,0 +1,17 @@
+package com.ruoyan.commom.exception;
+
+
+/**
+ * @Package: com.ruoyan.commom.exception
+ * @ClassName: checkSuperAdminException
+ * @Author: ruoyan1998
+ * @CreateTime: 2021/12/19 9:12
+ * @Description:
+ */
+public class CheckSuperAdminException extends RuntimeException
+{
+    public CheckSuperAdminException(String message)
+    {
+        super(message);
+    }
+}
