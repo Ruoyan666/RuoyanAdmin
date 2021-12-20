1eacb5bf0b93a9f44a1ac3dee46ac6bfdf3ba0f9
ruoyan <java@ruoyan.com>
2021-12-20 08:38:30 +0800

    新增首页内容

--- ruoyanadmin-vue/src/views/Index.vue
+++ ruoyanadmin-vue/src/views/Index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>main</div>
+  <div style="text-align: center">
+    <h2>欢迎来到RuoyanAdmin管理系统</h2>
+    <el-image :src="require('@/assets/index.jpg')" style="height: 300px; width: 500px"></el-image>
+
+    <p>学习开发</p>
+    <p>若言Ruoyan</p>
+    <div>透着光 彩绘的玻璃窗  装饰着哥德式教堂</div>
+    <div style="margin-left: 400px">-----《布拉格广场》</div>
+  </div>
 </template>
 
 <script>
