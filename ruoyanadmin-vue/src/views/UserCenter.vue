a58e209c86628920bb2506b92be01b6c74b5edf0
ruoyan <java@ruoyan.com>
2021-12-20 16:24:36 +0800

    新增了更改用户头像界面（未完成功能）

--- ruoyanadmin-vue/src/views/UserCenter.vue
+++ ruoyanadmin-vue/src/views/UserCenter.vue
@@ -1,7 +1,30 @@
 <template>
-  <div style="text-align: center;">
+
+
+  <div style="text-align: center">
     <h2>你好！{{ userInfo.username }} 用户</h2>
 
+    <el-col :xl="6" :lg="7" style="float: left;margin-right: 100px">
+      <h3>修改头像</h3>
+    <el-upload
+        class="avatar-uploader"
+        action="#"
+        :show-file-list="false"
+        :on-success="handleAvatarSuccess"
+        :before-upload="beforeAvatarUpload"
+        :http-request="avatarUpdate"
+    >
+      <img v-if="userInfo.avatar" :src="userInfo.avatar" class="avatar">
+      <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+    </el-upload>
+    </el-col>
+
+    <el-col :span="1" style="align-items: center">
+      <el-divider direction="vertical"></el-divider>
+    </el-col>
+
+    <el-col ::xl="6" :lg="7" style="float: right;margin-right: 180px">
+      <h3>修改密码</h3>
     <el-form :model="passForm" status-icon :rules="rules" ref="passForm" label-width="100px">
       <el-form-item label="旧密码" prop="currentPassword">
         <el-input type="password" v-model="passForm.currentPassword" autocomplete="off"></el-input>
@@ -17,6 +40,7 @@
         <el-button @click="resetForm('passForm')">重置</el-button>
       </el-form-item>
     </el-form>
+    </el-col>
   </div>
 </template>
 
@@ -36,7 +60,9 @@ export default {
     };
     return {
       userInfo: {
-
+        id:"",
+        username:"",
+        avatar:"",
       },
       passForm: {
         newPassword: '',
@@ -55,7 +81,8 @@ export default {
         currentPassword: [
           { required: true, message: '请输入当前密码', trigger: 'blur' },
         ]
-      }
+      },
+      imageUrl: ''
     }
   },
   created() {
@@ -91,7 +118,38 @@ export default {
     },
     resetForm(formName) {
       this.$refs[formName].resetFields();
-    }
+    },
+    handleAvatarSuccess(res, file) {
+      this.userInfo.avatar = URL.createObjectURL(file.raw);
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === 'image/jpeg';
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error('上传头像图片只能是 JPG 格式!');
+      }
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!');
+      }
+      return isJPG && isLt2M;
+    },
+    avatarUpdate(){
+      const userId = this.userInfo.id;
+
+      this.$axios.post("/sys/avatarUpdate/"+userId, this.userInfo.username,
+          { headers: {'Content-Type': 'multipart/form-data;'}}).then(res =>{
+
+        this.$message({
+          showClose: true,
+          message: '恭喜你，操作成功',
+          type: 'success',
+          onClose:() => {
+            this.getUserInfo()
+          }
+        });
+      })
+    },
   }
 }
 </script>
@@ -100,5 +158,31 @@ export default {
 .el-form {
   width: 420px;
   margin: 50px auto;
+}
+.el-divider{
+  height: 350px;
+}
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 100px;
+  height: 100px;
+  line-height: 100px;
+  text-align: center;
+}
+.avatar {
+  width: 100px;
+  height: 100px;
+  display: block;
 }
 </style>
